f44cefb3cebb670280367589d9463055e2a6c8ed
ian-dqn <ian.duquennoi@gmail.com>
2023-12-12 17:12:51 +0100

    [FEAT] modify user prefs

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -67,7 +67,7 @@ exports.putUser = (req, res, next) => {
 
     // Exemple : Récupération du nouveau mot de passe depuis le corps de la requête
     const newPassword = req.body.password;
-
+    // const newPreferences = req.body.newsPref;
 
     // Hasher le nouveau mot de passe avant de le stocker
     bcrypt.hash(newPassword, 10)
@@ -75,7 +75,7 @@ exports.putUser = (req, res, next) => {
             // Utilisez le mot de passe haché pour mettre à jour l'utilisateur
             User.findByIdAndUpdate(
                 userId,
-                { password: hashedPassword },
+                { password: hashedPassword, articlesPrefs: newPreferences },
                 { new: true, runValidators: true }
             )
                 .then((updatedUser) => {
