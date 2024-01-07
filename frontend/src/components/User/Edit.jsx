70de2b61c709c439e3c3410583c7a06e94e613b5
Axel Ouahmane <axel.ouahmane@gmail.com>
2024-01-07 01:30:40 +0100

    Edit user

--- frontend/src/components/User/Edit.jsx
+++ frontend/src/components/User/Edit.jsx
@@ -1,13 +1,13 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
-import useFetch from '../../hooks/useFetch';
+import Sidebar from '../../templates/Header/Sidebar';
+import { toast } from 'react-toastify';
 
 const Edit = () => {
 
   const {id} = useParams() ;
   console.log(id)
-  //const {data, loading, error, reFetch} = useFetch('http://localhost:5000/api/cryptodata')
   const dataString = localStorage.getItem('data');
   const data = JSON.parse(dataString);
   const token = data.token
@@ -37,37 +37,44 @@ const Edit = () => {
     axios.put(`http://localhost:4000/api/auth/${id}`,{
       password:values.password
     },{ headers: {'Content-Type': 'application/json','Authorization': `Bearer ${token}`} })
-      .then(res=>{navigate("/") }).catch(err=>console.log(err))     
+      .then(res=>{
+        toast.success('Your Profil has been updated !', {
+          position: toast.POSITION.TOP_RIGHT
+  
+        });
+        navigate(`/edit/${id}`)
+       }).catch(err=>console.log(err))     
   }
   return (
     <>
-      <div className="login-container">
+      <div className="container">
+      <div className='aside-profile'>
+      <Sidebar />
 
-        <h1>Mettre à jour</h1>
+<form onSubmit={handelSubmit} className='aside-profile-form'>
+    <label htmlFor="email">E-mail:</label>
+    <input
+      type="email"
+      id="email"
+      name="email"
+      value={values.email}
+      readonly
+      required
+    />
 
-        <form onSubmit={handelSubmit}>
-          <label htmlFor="email">E-mail:</label>
-          <input
-            type="email"
-            id="email"
-            name="email"
-            value={values.email}
-            readonly
-            required
-          />
+    <label htmlFor="password">Password:</label>
+    <input
+      type="password"
+      id="password"
+      name="password"
+      value={values.password}
+      required
+      onChange={e=>setValues({...values,password:e.target.value})}
+    />
 
-          <label htmlFor="password">Password:</label>
-          <input
-            type="password"
-            id="password"
-            name="password"
-            value={values.password}
-            required
-            onChange={e=>setValues({...values,password:e.target.value})}
-          />
-
-          <button type="submit">Modifier</button>
-        </form>
+    <button type="submit">Modifier</button>
+  </form>
+      </div>
       </div>
     </>
   );
---
+++ frontend/src/templates/Header/Sidebar.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Nav, Navbar, NavDropdown } from 'react-bootstrap';
+import "../../asset/css/Aside.css";
+import { Link } from 'react-router-dom';
+
+const Sidebar = () => {
+    const userString = localStorage.getItem("user");
+    const user = userString ? JSON.parse(userString) : null;
+    return (
+        <Navbar variant="dark" className="flex-column asidBar" >
+            <Nav  className="flex-column">
+                <Nav.Item className='navItem'>
+                    <Link className='link-aside' to={`/edit/${user._id}`} active>
+                        Profil
+                    </Link>
+                </Nav.Item>
+                <Nav.Item className='navItem' >
+                    <Link className='link-aside' to="/myCrypto" >
+                        Mes Cryptos
+                    </Link>
+                </Nav.Item>
+                <Nav.Item  className='navItem'>
+                    <Link className='link-aside' to="/news" >
+                        Actualités
+                    </Link>
+                </Nav.Item>
+                <Nav.Item className='navItem'>
+                    <Link className='link-aside' href="/" active>
+                        Echanges
+                    </Link>
+                </Nav.Item>
+            </Nav>
+        </Navbar>
+    );
+};
+
+export default Sidebar;
