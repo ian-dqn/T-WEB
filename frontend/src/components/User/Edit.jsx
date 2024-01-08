cd2197ef4edaf567293ef8c5d14cd4a6101bde6a
Your Name <dantouati18@gmail.com>
2024-01-08 10:47:36 +0100

    Revert "Edit user"
    This reverts commit 70de2b61c709c439e3c3410583c7a06e94e613b5.

--- frontend/src/components/User/Edit.jsx
+++ frontend/src/components/User/Edit.jsx
@@ -1,13 +1,13 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
-import Sidebar from '../../templates/Header/Sidebar';
-import { toast } from 'react-toastify';
+import useFetch from '../../hooks/useFetch';
 
 const Edit = () => {
 
   const {id} = useParams() ;
   console.log(id)
+  //const {data, loading, error, reFetch} = useFetch('http://localhost:5000/api/cryptodata')
   const dataString = localStorage.getItem('data');
   const data = JSON.parse(dataString);
   const token = data.token
@@ -37,44 +37,37 @@ const Edit = () => {
     axios.put(`http://localhost:4000/api/auth/${id}`,{
       password:values.password
     },{ headers: {'Content-Type': 'application/json','Authorization': `Bearer ${token}`} })
-      .then(res=>{
-        toast.success('Your Profil has been updated !', {
-          position: toast.POSITION.TOP_RIGHT
-  
-        });
-        navigate(`/edit/${id}`)
-       }).catch(err=>console.log(err))     
+      .then(res=>{navigate("/") }).catch(err=>console.log(err))     
   }
   return (
     <>
-      <div className="container">
-      <div className='aside-profile'>
-      <Sidebar />
+      <div className="login-container">
 
-<form onSubmit={handelSubmit} className='aside-profile-form'>
-    <label htmlFor="email">E-mail:</label>
-    <input
-      type="email"
-      id="email"
-      name="email"
-      value={values.email}
-      readonly
-      required
-    />
+        <h1>Mettre à jour</h1>
 
-    <label htmlFor="password">Password:</label>
-    <input
-      type="password"
-      id="password"
-      name="password"
-      value={values.password}
-      required
-      onChange={e=>setValues({...values,password:e.target.value})}
-    />
+        <form onSubmit={handelSubmit}>
+          <label htmlFor="email">E-mail:</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={values.email}
+            readonly
+            required
+          />
 
-    <button type="submit">Modifier</button>
-  </form>
-      </div>
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            value={values.password}
+            required
+            onChange={e=>setValues({...values,password:e.target.value})}
+          />
+
+          <button type="submit">Modifier</button>
+        </form>
       </div>
     </>
   );
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,16 +3,14 @@ import ReactDOM from 'react-dom/client';
 
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
-import { ToastContainer, } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+//import 'bootstrap/dist/css/bootstrap.css'
+// import 'bootstrap/dist/js/bootstrap.min.js'
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <ToastContainer />
     <App />
   </React.StrictMode>
 );
